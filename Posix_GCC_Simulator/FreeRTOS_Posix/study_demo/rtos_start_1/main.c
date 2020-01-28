0eeeddfcb753674b9b1d28077a4462af726d7740
jinhaotutu <mailjinhao@163.com>
2020-01-28 15:19:03 +0800

    update some

--- Posix_GCC_Simulator/FreeRTOS_Posix/study_demo/rtos_start_1/main.c
+++ Posix_GCC_Simulator/FreeRTOS_Posix/study_demo/rtos_start_1/main.c
@@ -1,23 +1,54 @@
-#include "stdio.h"
-#include "string.h"
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Tuu
+  * @version V1.0.0
+  * @date    2020-01-28
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  * Freertos run in the linux
+  * 1.first_demo: rtos_start_1 ---> rtos_start_1.bin
+  ******************************************************************************
+  */
 
+/* Includes ------------------------------------------------------------------*/
 #include "FreeRTOS.h"
 #include "task.h"
 
-#define __FILENAME__ (strrchr(__FILE__, '/') ? (strrchr(__FILE__, '/') + 1):__FILE__)
-#define os_printf(_fmt_, ...) \
-        printf("[log]%s:%d | "_fmt_"\r\n", __FILENAME__, __LINE__, ##__VA_ARGS__)
+#include "config.h"
 
+/* Private define ------------------------------------------------------------*/
+
+/* Private typedef -----------------------------------------------------------*/
+
+/* Private macro -------------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
 static TaskHandle_t xTask_creat;
 
+/* Private function prototypes -----------------------------------------------*/
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  freertos first creat task
+  * @note   This function is used to creat app task and delect self.
+  * @param  *p
+  * @param  None
+  * @retval None
+  */
 static void creat_task(void *p)
 {
     os_printf("%s", __FUNCTION__);
     int cnt = 0;
 
-    /* creat app task in this */
+    /* creat app task in this 在这里创建应用任务 */
 
 
+    /* creat app task in this 在这里创建应用任务 */
+
+    /* delay task 延时退出，并删除本任务 */
     while(1){
         os_printf("this is creat task:idle-%d", cnt++);
         vTaskDelay(1000);
@@ -32,12 +63,20 @@ static void creat_task(void *p)
     vTaskDelete(xTask_creat);
 }
 
+/**
+  * @brief  main
+  * @note   None
+  * @param  None
+  * @param  None
+  * @retval None
+  */
 int main(void)
 {
     portBASE_TYPE xReturn = pdPASS;
 
     os_printf("Freertos v10.2.1 start ");
 
+    /* first creat task in this 创建rtos第一个任务，用于创建其他任务 */
     xReturn = xTaskCreate(  (TaskFunction_t )creat_task,
                             (const char *   )"creat_task",
                             (unsigned short )128,
@@ -49,6 +88,7 @@ int main(void)
         return -1;
     }
 
+    /* start task 开启任务调度 */
     vTaskStartScheduler();
 
     while(1){
@@ -56,3 +96,10 @@ int main(void)
 
     return 0;
 }
+
+
+/**
+  * @}
+  */
+
+/******************************** (C) Tuu *********************END OF FILE****/
